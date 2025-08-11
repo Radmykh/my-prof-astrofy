d1fbec4a04a96e59b5b982f4adb217236869dcca
radmykh <radmykh@gmail.com>
2025-08-11 23:56:41 +0300

    Редагування2. 11,08 -фото+назва

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'MechMentor | Радченко Михайло';
+export const SITE_DESCRIPTION = 'Репетитор з технічної механіки. Допомога з вивченням алгоритмів розв\'язання задач.';
+export const SITE_URL = 'https://radmykh.pp.ua';
+export const SITE_AUTHOR = 'Радченко Михайло';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true

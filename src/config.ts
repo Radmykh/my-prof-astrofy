48493c07c3471ca6326f7426170b45f5cc92cb9b
radmykh <radmykh@gmail.com>
2025-09-13 14:07:16 +0300

    Відмова від переіменування

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'MechMentor | Радченко Михайло';
-export const SITE_DESCRIPTION = 'Репетитор з технічної механіки. Допомога з вивченням алгоритмів розв\'язання задач.';
+export const SITE_DESCRIPTION = 'Викладач з технічної механіки. Допомога з вивченням алгоритмів розв\'язання задач.';
 export const SITE_URL = 'https://radmykh.pp.ua';
 export const SITE_AUTHOR = 'Радченко Михайло';
 export const GENERATE_SLUG_FROM_TITLE = true
